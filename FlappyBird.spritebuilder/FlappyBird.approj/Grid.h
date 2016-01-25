8fdb2fb1f5b9e9b18f46085d7f940368cdd6bb2d
KingTripp <terrell.guinn@lander.edu>
2016-01-25 08:39:27 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ FlappyBird.spritebuilder/FlappyBird.approj/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  
+//
+//  Created by Tripp Guinn on 6/18/15.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ FlappyBird.spritebuilder/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  
+//
+//  Created by Tripp Guinn on 6/18/15.
+//
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
